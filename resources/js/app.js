c0666960709ab044eee8955009c7d1a53a2a8efe
giorgi.b <100768279+giobutiashvili@users.noreply.github.com>
2024-06-05 08:36:16 +0400

    change some files

--- resources/js/app.js
+++
@@ -1,8 +0,0 @@
-import "./bootstrap";
-import { createApp } from "vue";
-import app from "./vue/app.vue";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-
-library.add(fas);
-createApp(app).mount("#app");
